c3e6a9382f6ba03481ffa7f228dd9ff0b1b0ece1
= <smadhra@csc.com>
2018-02-27 12:24:09 +0530

    added

--- com.ibm.smadhra/src/main/java/com/ibm/smadhra/B.java
+++ com.ibm.smadhra/src/main/java/com/ibm/smadhra/B.java
@@ -5,7 +5,7 @@ public class B {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Suresh");
-		System.out.println("Suresh Madhra");
+		System.out.println("Suresh Madhra.");
 		
 	}
 
