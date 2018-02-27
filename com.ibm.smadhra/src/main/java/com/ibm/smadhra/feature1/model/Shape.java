5b308edab856c5bdf2bc5ab2f85d111833b158e8
= <=>
2018-02-27 17:24:41 +0530

    added 3 line

--- com.ibm.smadhra/src/main/java/com/ibm/smadhra/feature1/model/Shape.java
+++ com.ibm.smadhra/src/main/java/com/ibm/smadhra/feature1/model/Shape.java
@@ -5,6 +5,7 @@ public class Shape {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is new line added by suresh madhra");
+System.out.println("This is new line added by suresh madhra ");
 	}
 
 }
