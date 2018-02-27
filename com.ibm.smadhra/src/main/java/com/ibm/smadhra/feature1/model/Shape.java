eec7290620c8c7a6adb33e4236c3b9d9e3895690
= <=>
2018-02-27 21:43:23 +0530

    added in feature 2

--- com.ibm.smadhra/src/main/java/com/ibm/smadhra/feature1/model/Shape.java
+++ com.ibm.smadhra/src/main/java/com/ibm/smadhra/feature1/model/Shape.java
@@ -12,6 +12,8 @@ System.out.println("This is new line added by suresh madhra 1 ");
 System.out.println("This is new line added by suresh madhra 2 ");
 		System.out.println("this line added feature 1");
 		System.out.println("this line added feature 1");
+		
+		System.out.println("this line added feature 1.. new line added");
 	}
 
 }
