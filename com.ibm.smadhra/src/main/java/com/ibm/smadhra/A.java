1511af56a17ed086348291e2a4937942eede5f3b
= <smadhra@csc.com>
2018-02-27 12:21:15 +0530

    Added B.class

--- com.ibm.smadhra/src/main/java/com/ibm/smadhra/A.java
+++ com.ibm.smadhra/src/main/java/com/ibm/smadhra/A.java
@@ -3,5 +3,7 @@ package com.ibm.smadhra;
 public class A {
 public static void main(String ar[]) {
 	System.out.println("Suresh");
+	System.out.println("Suresh Madhra");
+	
 }
 }
---
+++ com.ibm.smadhra/src/main/java/com/ibm/smadhra/B.java
@@ -0,0 +1,12 @@
+package com.ibm.smadhra;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Suresh");
+		System.out.println("Suresh Madhra");
+		
+	}
+
+}
