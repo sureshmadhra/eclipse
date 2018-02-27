2b67562efc1a502ea522d25e14a58d0fb89e9f54
= <=>
2018-02-27 12:25:57 +0530

    kkk

--- com.ibm.smadhra/src/main/java/com/ibm/smadhra/B.java
+++ com.ibm.smadhra/src/main/java/com/ibm/smadhra/B.java
@@ -4,7 +4,7 @@ public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Suresh");
+		System.out.println("Suresggh");
 		System.out.println("Suresh Madhra.");
 		
 	}
